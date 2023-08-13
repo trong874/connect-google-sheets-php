53d5bb85caae69b90b80c1ebdceb69f72d967e4b
trong-vdtsol <trong.pham@vdtsol.com>
2023-08-13 19:32:58 +0700

    update code

--- core/handleLogin.php
+++ core/handleLogin.php
@@ -1,41 +1,55 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require '../vendor/autoload.php';
+
+const HOME_URL = '../index.php';
 
 date_default_timezone_set('Asia/Ho_Chi_Minh');// Set time zone
 
 
 $client = new Google_Client();
 $client->setApplicationName('Users');
-$client->setScopes([Google_Service_Sheets::SPREADSHEETS_READONLY]);
-$client->setAuthConfig('./credentials.json');
+$client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+$client->setAuthConfig('../credentials.json');
 $client->setAccessType('offline');
+$client->setIncludeGrantedScopes(true);
 
 $service = new Google_Service_Sheets($client);
 $spreadsheetId = '1kDiRcAD6fFvq7z9LF7aOj5SaZW3RCnnqVMDyBE6qLmA'; // ID file sheet
-$range = 'Users!A3:B'; // Tên trang cần lấy dữ liệu
+$range = 'Users!A2:B'; // Tên trang cần lấy dữ liệu
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
 $accounts = $response->getValues();
 
-$redirect_range = 'Users!B1';
-$response = $service->spreadsheets_values->get($spreadsheetId, $redirect_range);
-[[$redirect_url]] = $response->getValues();
-
 session_start();
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
 
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
-
     foreach ($accounts as $account) {
         if (!$account) continue;
         [$db_username,$db_password] = $account;
         if ($db_username === $username) {
             if ($db_password === $password) {
-                $file_log = 'log.txt';
-                $current = file_get_contents($file_log);
-                $current .="Account: $username - " . 'Login at -> ' . date('d-m-Y H:i:s'). "\n";
-                file_put_contents($file_log, $current);
-                header('Location: '. $redirect_url);
+                $range = 'Logs!A:B';
+                $value = [
+                    [$username,date('d-m-Y H:i:s')],
+                ];
+
+                $body = new Google_Service_Sheets_ValueRange([
+                    'values' => $value
+                ]);
+
+                $params = [
+                    'valueInputOption' => 'RAW'
+                ];
+
+                $result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
+
+                $_SESSION['auth'] = ['username'=> $username,'check'=>true];
+
+                header('Location: '. HOME_URL);
+                die();
             }
             break;
         }
--- index.php
+++ index.php
@@ -1,55 +1,26 @@
-<!DOCTYPE html>
+<?php
+session_start();
+if (!isset($_SESSION['auth']['check'])) {
+    header('Location: '. './pages/login.php');
+    die();
+}
+
+?>
+
+<!doctype html>
 <html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Login V10</title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="./assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="./assets/css/util.css">
-    <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-<div class="limiter">
-    <div class="container-login100">
-        <div class="wrap-login100 p-t-50 p-b-90">
-            <form class="login100-form validate-form flex-sb flex-w" method="POST" action="./handle.php">
-                <span class="login100-form-title p-b-51">Login</span>
+<h2>
+    Hello: <?php echo $_SESSION['auth']['username'] ?>
+</h2>
 
-                <?php
-                session_start();
-                if ($_SESSION['err_message'] ?? false) {
-                    echo '<span style="color: #c80000;margin-bottom: 8px;">' . $_SESSION['err_message'] . ' <i class="fa fa-exclamation"></i></span>';
-                    unset($_SESSION['err_message']);
-                }
-                ?>
-
-                <div class="wrap-input100 validate-input m-b-16">
-                    <input class="input100" type="text" name="username" placeholder="Username" required>
-                    <span class="focus-input100"></span>
-                </div>
-                <div class="wrap-input100 validate-input m-b-16">
-                    <span class="btn-show-pass">
-                        <i class="fa fa-eye"></i>
-                    </span>
-                    <input class="input100" type="password" name="password" placeholder="Password" required>
-                    <span class="focus-input100"></span>
-                </div>
-                <div class="flex-sb-m w-full p-t-3 p-b-24">
-                    <div class="contact100-form-checkbox">
-                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-                        <label class="label-checkbox100" for="ckb1">Remember me</label>
-                    </div>
-                    <div>
-                        <a href="#" class="txt1">Forgot?</a>
-                    </div>
-                </div>
-                <div class="container-login100-form-btn m-t-17">
-                    <button class="login100-form-btn">Login</button>
-                </div>
-            </form>
-        </div>
-    </div>
-    <script src="./assets/js/main.js"></script>
+<a href="core/logout.php">Logout</a>
 </body>
 </html>
